9c6a56babe64dec4e76218b692de089928c16779
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-08-22 17:01:54 +0200

    Add basic scrollsmoother

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useLayoutEffect, useRef } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollSmoother } from 'gsap/ScrollSmoother';
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollSmoother);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const main = useRef();
+  const smoother = useRef();
+
+  const scrollTo = () => {
+    smoother.current.scrollTo('.box-c', true, 'center center');
+  };
+
+  useGSAP(
+    () => {
+      // create the smooth scroller FIRST!
+      smoother.current = ScrollSmoother.create({
+        smooth: 2, // seconds it takes to "catch up" to native scroll position
+        effects: true, // look for data-speed and data-lag attributes on elements and animate accordingly
+        normalizeScroll: true
+      });
+      
+      // pin box-c when it reaches the center of the viewport, for 300px
+      ScrollTrigger.create({
+        trigger: '.box-c',
+        pin: true,
+        start: 'center center',
+        end: '+=300',
+        markers: true,
+      });
+    },
+    { scope: main }
+  );
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="smooth-wrapper" ref={main}>
+        <div id="smooth-content">
+          <header className="header">
+            <h1 className="title">ScrollSmoother</h1>
+            <button className="button" onClick={scrollTo}>
+              Jump to C
+            </button>
+          </header>
+          <div className="box box-a gradient-green" data-speed="clamp(0.5)">
+            a
+          </div>
+          <div className="box box-b gradient-purple" data-speed="clamp(0.8)">
+            b
+          </div>
+          <div className="box box-c gradient-orange" data-speed="1.5">
+            c
+          </div>
+          <div className="line"></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
